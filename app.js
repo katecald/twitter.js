2055a537c753043c66c5032372bce6082be68169
Nitika Nadgar <nitiquita@gmail.com>
2017-03-07 11:16:12 -0500

    nunjucks?

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const volleyball = require('volleyball')
+const volleyball = require('volleyball');
+const nunjucks = require('nunjucks');
 
 const twitterApp = express();
 
-
+twitterApp.set('view engine', 'html'); // have res.render work with html files
+twitterApp.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
 
 
 twitterApp.listen(3000, function() {
@@ -15,6 +18,8 @@ twitterApp.get('/', function(req, res, next) {
     next()
 })
 
+
+
 twitterApp.use(function(req, res, next) {
     console.log(req.method + ' / ' + res.statusCode)
     next();
@@ -24,3 +29,48 @@ twitterApp.use('/special/', function(req, res, next) {
     console.log('You reached the special area')
     next();
 })
+
+
+
+// var env = nunjucks.configure('views');
+
+// function Person(name) {
+//     this.name = name;
+// }
+
+// var gandalf = new Person("Gandalf");
+// var frodo = new Person("Frodo");
+// var hermione = new Person("Hermione");
+
+// var people = {
+//     gandalf, 
+//     frodo, 
+//     hermione
+// }
+
+// var nJ = env.render('index.html', {title: "An Example"}, {people: people});
+
+
+// twitterApp.get('/views/index.html', function(req, res, next) {
+//     res.send(nJ)
+//     next()
+// })
+
+var locals = {//declaring a locals var to hold all the variable created with nunjucks
+    title: 'An Example',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+// var
+var page = nunjucks.render('index.html', locals);
+//callback doesn't render in he browser or the front end 
+
+twitterApp.get('/views/index.html', function(req, res, next) {
+    res.send(page)
+    next();
+})
+
+
